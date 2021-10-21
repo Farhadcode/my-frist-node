33ce9aa01229fe4d3465482bd25a70a0fa6d328a
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-10-21 19:39:07 +0600

    add  data

--- index.js
+++ index.js
@@ -26,6 +26,52 @@ app.get('/', (req, res) => {
 
 });
 
+const users = [
+    {
+        id: 0,
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@april.biz"
+    },
+    {
+        id: 1,
+        name: "aligzande",
+        username: "Bret",
+        email: "Sincere@april.biz"
+    },
+    {
+        id: 2,
+        name: "Goold man",
+        username: "Bret",
+        email: "Sincere@april.biz"
+    },
+    {
+        id: 3,
+        name: "Leanne bom",
+        username: "Bret",
+        email: "Sincere@april.biz"
+    },
+    {
+        id: 4,
+        name: "yeang hook",
+        username: "Bret",
+        email: "Sincere@april.biz"
+    },
+]
+
+
+app.get('/users/:id', (req, res) => {
+    // console.log(req.params.id);
+
+    const id = req.params.id;
+    const result = users[id];
+    res.send(result)
+});
+
+app.get('/users', (req, res) => {
+    res.send(users)
+});
+
 app.listen(port, () => {
     console.log('local host ', port);
 })
